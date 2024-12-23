c63fb92eae4311618544d05e2ee5e33819a60fbb
phuc <vanp2100@gmail.com>
2024-12-23 21:15:42 +0700

    no something

--- Chrismast/settings.py
+++ Chrismast/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7sw5hok*@z++5g204ke)^m@$ek&q#otvbznt)mn8uf#s5p3@kq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
